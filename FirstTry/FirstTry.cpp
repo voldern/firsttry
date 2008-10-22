04ce9f0cc75ab8aca1f55012f0c8c60a7c201cad
Espen Volden <voldern@hoeggen.net>
2008-10-22 23:13:42 +0200

    Made the main loop break if done == false

--- FirstTry/FirstTry.cpp
+++ FirstTry/FirstTry.cpp
@@ -14,7 +14,7 @@ namespace FirstTry
         this->init();
 
         printf("Starting main loop\n");
-        while (this->device->Update())
+        while (this->device->Update() && !this->done)
         {
             this->device->Clear(Frontend::Graphics::ClearBuffersAll, 0, 0, 1, 0, 1, 0);
         }
