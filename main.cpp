51f2ff7638259e68d348301038fb1bd8732058aa
Espen Volden <espenv@hardware.no>
2009-03-10 23:02:06 +0100

    Started from scratch

--- main.cpp
+++ main.cpp
@@ -1,23 +1,10 @@
-#include "FirstTry/FirstTry.h"
-#include "FirstTry/Scenes/Intro.h"
+#include <OpenFrontend2.h>
+#include <Frontend2Utils.h>
+#include <Frontend2Graphics.h>
+
+using namespace Frontend;
 
 int main()
 {
-    try
-    {
-        FirstTry::FirstTry FirstTry;
-
-        FirstTry::Scene::Intro* introScene = new FirstTry::Scene::Intro(FirstTry, 50);
-        FirstTry.scenes.Add(introScene);
-
-        printf("Starting FirstTry\n");
-        FirstTry.init();
-        FirstTry.run();
-    }
-    catch(Frontend::Exception e)
-    {
-        printf("Frontend exception: %s\n", e.GetMessage().CharStringCopy());
-    }
-
-    return 0;
+  return 0;
 }
