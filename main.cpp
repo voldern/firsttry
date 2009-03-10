e35c535e8972a365ade72b331f07ec678bd9201b
Espen Volden <espenv@hardware.no>
2009-03-10 23:16:59 +0100

    Some quick code to check that Frontend is working

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include <OpenFrontend2.h>
 #include <Frontend2Utils.h>
-#include <Frontend2Graphics.h>
 
 using namespace Frontend;
 
-int main()
+int main(int argc, char **argv)
 {
+  Utils::Simple::SimpleSetup setup(GUI::CreateNativeWindowManager(), Graphics::OpenFrontendGL2CG());
+  setup.SetResolution(800, 600);
+  setup.SetTitle("FirstTry");
+  setup.Start();
+
+  while (setup.Update()) 
+    {
+      // Loop
+    }
+
   return 0;
 }
