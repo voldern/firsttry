8517ed5c642f9fdd1627edbb29a92b880775c67b
Espen Volden <voldern@harry.(none)>
2009-03-15 22:36:23 +0100

    Moved scenes/FirstScene.h to engine/Scene.h

---
+++ engine/Scene.h
@@ -0,0 +1,17 @@
+#ifndef SCENE_H_INCLUDED
+#define SCENE_H_INCLUDED
+
+#include <OpenFrontend2.h>
+#include <Frontend2Utils.h>
+
+namespace Engine {
+  class Scene
+  {
+  private:
+    Frontend::Utils::Simple::SimpleSetup *setup;
+
+  
+  };
+}
+
+#endif // FIRST_SCENE_H_INCLUDED
