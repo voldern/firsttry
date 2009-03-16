39cc4a21da3cbd2bba5a84a5ab68096a3e4ab17e
Espen Volden <espenv@hardware.no>
2009-03-16 18:24:32 +0100

    Reindented the whole file

--- main.cpp
+++ main.cpp
@@ -11,33 +11,46 @@ using namespace Frontend::Utils::Simple;
 
 int main(int argc, char** argv)
 {
-  IO::StdOut().WriteTextLine("Setting up enviroment");
-  SimpleSetup setup(GUI::CreateNativeWindowManager(), Graphics::OpenFrontendGL2CG());
-  setup.SetResolution(800, 600);
-  setup.SetTitle("Hello Frontend");
-  setup.Start();
-
-  Keyboard keyboard(setup.GetWindow());
-
-  double startTime = setup.GetTime();
-  IO::StdOut().WriteTextLine("Starting main loop");
-  while (setup.Update())
+    SimpleSetup setup(GUI::CreateNativeWindowManager(), Graphics::OpenFrontendGL2CG());
+  
+    try
     {
-      // Clear the screen
-      setup.GetGraphicsDevice()->Clear(Graphics::ClearBuffersAll, 1, 0, 0, 0, 1, 0);
-      
-      // Check if the user has pressed ESC, in that case terminate the program successfully
-      if (keyboard.KeyDown(GUI::KeyEsc))
-          return 1;
+        IO::StdOut().WriteTextLine("Setting up enviroment");
+ 
+        setup.SetResolution(1024, 768);
+        setup.SetTitle("Hello Frontend");
+        setup.SetFileSystemRoot("data");
+        setup.Start();
 
-      // Display FPS if the user presses F (F1 untill I figure out how to check against ASCII keys)
-      if (keyboard.KeyDown(GUI::KeyF1))
-        IO::StdOut().WriteTextLine((Frontend::String)setup.GetFPS());
+        Keyboard keyboard(setup.GetWindow());
+
+        double startTime = setup.GetTime();
+        IO::StdOut().WriteTextLine("Starting main loop");
+
+        // Load the scenes
+        FirstTry::TestScene firstScene(setup);
+
+        while (setup.Update())
+        {
+            // Clear the screen
+            setup.GetGraphicsDevice()->Clear(Graphics::ClearBuffersAll, 1, 0, 0, 0, 1, 0);
       
-      // Run the test scene
-      FirstTry::TestScene firstScene(setup);
-      firstScene.run();
+            // Check if the user has pressed ESC, in that case terminate the program successfully
+            if (keyboard.KeyDown(GUI::KeyEsc))
+                return 1;
+
+            // Display FPS if the user presses F (F1 untill I figure out how to check against ASCII keys)
+            if (keyboard.KeyDown(GUI::KeyF1))
+                IO::StdOut().WriteTextLine((Frontend::String)setup.GetFPS());
+      
+            // Run the test scene
+            firstScene.run();
+        }
+    }
+    catch (Exception e)
+    {
+        setup.HandleException(e);
     }
 
-  return 1;
+    return 1;
 }
