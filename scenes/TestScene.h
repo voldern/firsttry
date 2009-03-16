1aa9cc001e822fcc4d2cac36709b521e549ac106
Espen Volden <espenv@hardware.no>
2009-03-16 18:24:39 +0100

    Made the test scene render a 3d model with textures

--- scenes/TestScene.h
+++ scenes/TestScene.h
@@ -4,17 +4,60 @@
 #include "../engine/Scene.h"
 
 using namespace Frontend;
+using namespace Frontend::Utils;
+using namespace Frontend::Utils::Simple;
 
 namespace FirstTry
 {
   class TestScene: public FirstEngine::Scene
-  {
-  public:
-    TestScene(Frontend::Utils::Simple::SimpleSetup &engine) : FirstEngine::Scene(engine) {}
-    void run() {
-      this->engine->GetGraphicsDevice()->Clear(Graphics::ClearBuffersAll, 0, 0, 0, 0, 1, 0);
-    }
-  };
+    {
+    private:
+      RScene *level;
+      RProgram *shader;
+      SimpleRenderer *renderer;
+
+      Keyboard *keyboard;
+      Mouse *mouse;
+    public:
+    TestScene(Frontend::Utils::Simple::SimpleSetup &engine) : FirstEngine::Scene(engine)
+        {
+          this->level = new RScene("level.DAE");
+          this->shader = new RProgram("shader.cg");
+
+          this->renderer = new SimpleRenderer(this->engine->GetGraphicsDevice());
+          this->renderer->SetProgram(*this->shader);
+          this->renderer->GetCamera()->SetPosition(Math::Vector3(0, 0, 5));
+
+          // Setup input
+          this->keyboard = new Keyboard(this->engine->GetWindow());
+          this->mouse = new Mouse(this->engine->GetWindow());
+        }
+
+      void run() 
+      {
+        this->engine->GetGraphicsDevice()->Clear(Graphics::ClearBuffersAll, 0.3, 0.35, 0.65, 0, 1, 0);
+
+        float walkSpeed = 50.0f;
+        float turnSpeed = 0.2f;
+        float frameTime = this->engine->GetFrameTime();
+
+        if (this->keyboard->KeyDown(GUI::KeyShift)) {
+            walkSpeed = 500.0f;
+            IO::StdOut().WriteTextLine("Super speed!");
+        }
+
+        if (this->keyboard->KeyDown('W')) this->renderer->GetCamera()->Move(frameTime * walkSpeed);
+        if (this->keyboard->KeyDown('S')) this->renderer->GetCamera()->Move(-frameTime * walkSpeed);
+        if (this->keyboard->KeyDown('A')) this->renderer->GetCamera()->Strafe(-frameTime * walkSpeed);
+        if (this->keyboard->KeyDown('D')) this->renderer->GetCamera()->Strafe(frameTime * walkSpeed);
+      
+        Math::Vector2i mouseDelta = this->mouse->Lock(this->engine->GetWindow());
+        this->renderer->GetCamera()->Turn(mouseDelta.x * turnSpeed);
+        this->renderer->GetCamera()->Pitch(- mouseDelta.y * turnSpeed);
+
+        this->renderer->Render(this->engine->GetState(), *this->level);
+      }
+    };
 }
 
 #endif // TEST_SCENE_H_INCLUDED
